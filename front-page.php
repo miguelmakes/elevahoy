59d4f2ab92828bfe22f72ecafabd050f178ef1ea
miguelmln <md253@protonmail.com>
2018-03-31 11:29:21 -0400

    Updating homepage for interim

--- front-page.php
+++ front-page.php
@@ -10,11 +10,12 @@
     <div id="welcome-wrap">
         <div id="welcome-box">
             <img src="http://elevahoy.com/wp-content/uploads/2016/11/elevahoy_logo.png" alt="" />
-            <h1 class="canvas-page-title">Alcanze al sueno Americano.</h1>
+            <h1 class="canvas-page-title">Simple, personalized, and free.</h1>
             <p>
-                Hola. Me llamo Miguel y quiero ayudar a personas aprender como hablar Ingles. No tienes que hacer una cuenta o pagar dinero. Este curso es totalmente gratis! Empieze hoy, eleva hoy.
+                ElevaHoy is an online platform specifically designed for native-Spanish speakers to learn how to speak English.
             </p>
-            <a href="http://elevahoy.com/capitulos/" class="btn btn-primary canvas-btn">Empieze ahora</a>
+            <br>
+            <p>May 5th, 2018</p>
         </div>
     </div>
 </div>
