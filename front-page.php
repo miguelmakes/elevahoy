294361c8127f393cfb8951fc786746c6cdcf6731
miguelmln <md253@protonmail.com>
2018-07-02 14:40:10 -0400

    front page + style, latest, 7.2

--- front-page.php
+++ front-page.php
@@ -15,7 +15,30 @@
                 ElevaHoy is an online platform specifically designed for native-Spanish speakers to learn how to speak English.
             </p>
             <br>
-            <p>May 5th, 2018</p>
+            <p>Launching June 22, 2018. Sign up to our emailing list to be the first to have access to elevahoy.</p>
+            <br>
+            <!-- Begin MailChimp Signup Form -->
+            <div id="mc_embed_signup">
+                <form action="https://elevahoy.us18.list-manage.com/subscribe/post?u=80ee08f9a2035a546d1925b5f&amp;id=ae03980b23" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                        <div class="col-md-7 col-sm-12">
+                            <div class="mc-field-group">
+                            	<label for="mce-EMAIL" hidden>Email Address </label>
+                            	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="your email">
+                            </div>
+                            <div id="mce-responses" class="clear">
+                        		<div class="response" id="mce-error-response" style="display:none"></div>
+                        		<div class="response" id="mce-success-response" style="display:none"></div>
+                        	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        </div>
+                    	<div class="col-md-5 col-sm-12">
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_80ee08f9a2035a546d1925b5f_ae03980b23" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!--End mc_embed_signup-->
         </div>
     </div>
 </div>
